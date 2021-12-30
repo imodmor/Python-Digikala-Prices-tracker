330fb72323a0f7b839b2b635470085c676b87bca
Mohammad Moradi <mohammad.moradi@outlook.com>
2021-12-30 17:31:36 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.digikala.com/product/dkp-6459793/%DA%AF%D9%88%D8%B4%DB%8C-%D9%85%D9%88%D8%A8%D8%A7%DB%8C%D9%84-%D8%A7%D9%BE%D9%84-%D9%85%D8%AF%D9%84-iphone-13-%D8%AF%D9%88-%D8%B3%DB%8C%D9%85-%DA%A9%D8%A7%D8%B1%D8%AA-%D8%B8%D8%B1%D9%81%DB%8C%D8%AA-128-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA-%D9%88-%D8%B1%D9%85-4-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA'
+
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
+}
+
+page = requests.get(url, headers=header)
+
+res = BeautifulSoup(page.content, 'html.parser')
+
+product_title = res.find('h1', {"class": "c-product__title"}).get_text()
+product_price = res.find('div', {"class": "c-product__seller-price-pure"}).get_text()
+
+print(product_price.strip())
